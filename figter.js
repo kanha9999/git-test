899642d6cd20932d667fcbea26fc592438d6e764
kanha9999 <113540927+kanha9999@users.noreply.github.com>
2022-09-21 11:15:25 +0700

    Update figter.js

--- figter.js
+++ figter.js
@@ -3,3 +3,9 @@ console.log(message);
 
 let fruit = ["Apple","Banana"];
 console.log(fruit)
+
+let nameFieldCheck='true';
+let ageFieldCheck='false';
+let isGreater =4<1 ;
+
+console.log(isGreater);
